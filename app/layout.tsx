0424e3061484123183cbfb4474355e01cade76cf
husamXA1 <husamxm0@gmail.com>
2025-01-21 12:15:05 +0200

    add frontendmentor favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,6 +13,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="shortcut icon" href="/assets/favicon.png" type="image/x-icon" />
+      </head>
       <body>
         {children}
       </body>
